19be632c052cab4195083f88159ac23d4dde4e9b
Anbarasan.s <anbarasansenthilkumar91@gmail.com>
2026-07-08 08:09:46 +0530

    Commit message:

--- Sofnum.java
+++ Sofnum.java
@@ -1,20 +1,16 @@
-
 import java.util.Scanner;
-public class Eonum {
+public class Sofnum {
    public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
    System.out.println("enter the num");
    int num = sc.nextInt();
-   
-   if (num%2==0) {
-     System.out.println("even");
-}
-   else {
-     System.out.println("odd");
-     sc.close();
-}    
-      }
-     
-}
-    
+int sum=0;
+   for (int i=1;i<=num;i++) {
+    sum=sum+i;
 
+   }
+   System.out.println("sum is " + sum);
+   sc.close();
+}
+
+         }
