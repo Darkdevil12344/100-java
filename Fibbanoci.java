7666cd689577d88b1e1e89829daf566367a76c83
Anbarasan.s <anbarasansenthilkumar91@gmail.com>
2026-07-14 21:54:46 +0530

    commit

---
+++ Fibbanoci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Fibbanoci {
+   public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("enter the num");
+   int num = sc.nextInt();
+   int x=0;
+   int y=1;
+   int z;
+   System.out.println(x);
+   System.out.println(y);
+      for(int i=1;i<=num;i++) {
+      z=x+y;
+      x=y;
+      y=z;
+
+      System.out.println(z);
+    }
+      }
+   }
+
+         
---
+++ Fibbanocin.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Fibbanocin {
+   public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("enter the num");
+   int num = sc.nextInt();
+   int x=0;
+   int y=1;
+   int z=0;
+      for(int i=1;i<=num;i++) {
+      z=x+y;
+      x=y;
+      y=z;
+
+      
+    }
+    System.out.println(z);
+      }
+    
+   }
+
+         
